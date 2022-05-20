8ec89f7ded0726992a4d50e7ed0452ae43648bcb
pep <sofia.boughriet@hotmail.com>
2022-05-20 12:20:13 +0200

    Score tmp....

--- Evaluation-empty/Assets/Scripts/ScoreDisplayer.cs
+++ Evaluation-empty/Assets/Scripts/ScoreDisplayer.cs
@@ -1,8 +1,17 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class ScoreDisplayer : MonoBehaviour
 {
+
+    private TextMeshPro tmPro;
+
+    private void Awake()
+    {
+        tmPro = GetComponent<TextMeshPro>();
+    }
     public void SetScore(int score)
     {
+       
     }
 }
