42df43ca93a916b69bf70a1e0a779e7369bd986c
pep <sofia.boughriet@hotmail.com>
2022-05-20 12:15:24 +0200

    bouton de reset

--- Evaluation-empty/Assets/Scripts/RestartButton.cs
+++ Evaluation-empty/Assets/Scripts/RestartButton.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Button))]
 public class RestartButton : MonoBehaviour
 {
     public void RestartScene()
     {
+        SceneManager.LoadScene("Game");
     }
 }
